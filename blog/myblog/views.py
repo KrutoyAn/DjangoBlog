13fcca55efecccaef5c006ed2134b1b8fc289202
andrei <andrei.krutoy1997@gmail.com>
2022-03-26 15:02:57 +0100

    new update

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
+from .forms import SigUpForm, SignInForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
+from .forms import FeedBackForm
+from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
+
 
 # Create your views here.
 class MainView(View):
@@ -13,3 +21,77 @@ class MainView(View):
         page_obj = paginator.get_page(page_number)
 
         return render(request, 'myblog/home.html', context={'page_obj': page_obj})
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={'post': post})
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SigUpForm()
+        return render(request, 'myblog/signup.html', context={'form': form,})
+
+    def post(self, request,  *args, **kwargs):
+        form = SigUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return  HttpResponseRedirect('/')
+            return render(request, 'myblog/signup.html', context={'form:form'})
+
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+
+class FeedBackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+            'title': 'Написать мне'
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'От {name} | {subject}', message, from_email, ['andrei.nikiti@mail.ru'])
+            except BadHeaderError:
+                return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('success')
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+        })
+
+
+class SuccessView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'myblog/success.html', context={
+            'title': 'Спасибо'
+        })
